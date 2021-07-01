821a86cee066bf3ebe29c1ac61f4d81dfd87e907
HADMARINE <tsx@kakao.com>
2021-07-01 22:31:21 +0900

    Update : ask when overwritng

--- src/cli/register.ts
+++ src/cli/register.ts
@@ -1,11 +1,29 @@
 import fs from "fs";
 import chalk from "chalk";
 import u from "./util";
+import prompts from "prompts";
 
-export default function register(args: Record<string, any>) {
+export default async function register(args: Record<string, any>) {
   const config = u.config.get(args.config);
   const privkey = u.keyResolver(args.key, args.keyfile);
   const loc = args.dir;
+
+  if (Object.keys(config).indexOf(loc) !== -1) {
+    console.log(`${chalk.cyan(`!`)} File already exists.`);
+
+    const response = await prompts({
+      type: "confirm",
+      name: "value",
+      message: "Do you want to overrite?",
+    });
+
+    if (response.value) {
+      delete config[loc];
+    } else {
+      console.log(`${chalk.red(`✘`)} Aborted.`);
+      return;
+    }
+  }
 
   try {
     const file = fs.readFileSync(loc).toString();
