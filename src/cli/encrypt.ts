45316a3e36542a5e22a98de0d2e060f4a95c0cc3
hadmarine <contact@hadmarine.com>
2023-09-06 19:34:22 +0200

    Bugfix: encrypt one by one

--- src/cli/encrypt.ts
+++ src/cli/encrypt.ts
@@ -10,33 +10,35 @@ export default async function encrypt(args: Record<string, any>) {
   const privkey = await u.keyResolver(args.key, args.keyfile);
   const locs = args.filePath as string[];
 
-  locs.forEach(async (loc) => {
-    if (Object.keys(config).indexOf(loc) !== -1) {
-      console.log(`${chalk.cyan(`!`)} File already exists.`);
+  await Promise.all(
+    locs.map(async (loc) => {
+      if (Object.keys(config).indexOf(loc) !== -1) {
+        console.log(`${chalk.cyan(`!`)} File already exists.`);
 
-      const response = await prompts({
-        type: 'confirm',
-        name: 'value',
-        message: 'Do you want to overwrite?',
-      });
+        const response = await prompts({
+          type: 'confirm',
+          name: 'value',
+          message: 'Do you want to overwrite?',
+        });
 
-      if (response.value) {
-        delete config[loc];
-      } else {
-        console.log(`${chalk.red(`✘`)} Aborted.`);
-        return;
+        if (response.value) {
+          delete config[loc];
+        } else {
+          console.log(`${chalk.red(`✘`)} Aborted.`);
+          return;
+        }
       }
-    }
 
-    try {
-      const file = fs.readFileSync(loc).toString();
-      config[loc] = u.encrypt.encrypt(file, privkey);
-    } catch (e) {
-      throw e;
-    }
+      try {
+        const file = fs.readFileSync(loc).toString();
+        config[loc] = u.encrypt.encrypt(file, privkey);
+      } catch (e) {
+        throw e;
+      }
 
-    await u.appendPreciseStringOnFileIfExists('.gitignore', loc);
-  });
+      await u.appendPreciseStringOnFileIfExists('.gitignore', loc);
+    }),
+  );
 
   u.config.set(args.config, config);
 
